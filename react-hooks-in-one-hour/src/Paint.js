3186e490bfa54b157909fae66d8d5d510d1c8812
antdp425 <antdp425@aol.com>
2021-01-27 08:25:51 -0500

    completes useRef

--- react-hooks-in-one-hour/src/Paint.js
+++ react-hooks-in-one-hour/src/Paint.js
@@ -1,41 +1,52 @@
-import React, {useState, useEffect} from 'react'
-import Name from './Name'
-import ColorPicker from './ColorPicker'
-import randomColor from 'randomcolor'
-import WindowSize from './WindowSize'
+import React, { useState, useEffect, useRef } from "react";
+import Name from "./Name";
+import ColorPicker from "./ColorPicker";
+import randomColor from "randomcolor";
+import WindowSize from "./WindowSize";
+import Canvas from "./Canvas";
 
 function Paint() {
-   const [colors, setColors] = useState([])
-   const [activeColor, setActiveColor] = useState(null)
-   const getColors = () => {
-      const baseColor = randomColor().slice(1);
-      fetch(`https://www.thecolorapi.com/scheme?hex=${baseColor}&mode=monochrome`)
-      .then(res => res.json())
-      .then(res => {
-        setColors(res.colors.map(color => color.hex.value))
-        setActiveColor(res.colors[0].hex.value)
-      })
-    }
+  // creating our Ref
+  const [colors, setColors] = useState([]);
+  const [activeColor, setActiveColor] = useState(null);
+  const getColors = () => {
+    const baseColor = randomColor().slice(1);
+    fetch(`https://www.thecolorapi.com/scheme?hex=${baseColor}&mode=monochrome`)
+      .then((res) => res.json())
+      .then((res) => {
+        setColors(res.colors.map((color) => color.hex.value));
+        setActiveColor(res.colors[0].hex.value);
+      });
+  };
 
-    useEffect(getColors, [])
+  useEffect(getColors, []);
+  const headerRef = useRef({ offsetHeight: 0 })
 
-   return (
-     <div className="app">
-      <header style={{ borderTop: `10px solid ${activeColor}` }}>
-      <div className="app">
-        <Name />
-      </div>
-      <div style={{ marginTop: 10 }}>
-        <ColorPicker
-          colors={colors}
-          activeColor={activeColor}
-          setActiveColor={setActiveColor}
-        />
-      </div>
-    </header>
-    <WindowSize />
+  return (
+    <div className="app">
+      {/* Adding ref to header tag so that we can access it with variable */}
+      {/* THe DOM element will be at header.current */}
+      <header
+        ref={headerRef}
+        style={{ borderTop: `10px solid ${activeColor}` }}
+      >
+        <div className="app">
+          <Name />
+        </div>
+        <div style={{ marginTop: 10 }}>
+          <ColorPicker
+            colors={colors}
+            activeColor={activeColor}
+            setActiveColor={setActiveColor}
+          />
+        </div>
+      </header>
+      {activeColor && (
+        <Canvas color={activeColor} height={window.innerHeight - headerRef.current.offsetHeight} />
+      )}
+      <WindowSize />
     </div>
-   )
+  );
 }
 
-export default Paint
+export default Paint;
